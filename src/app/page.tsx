4b746b3744ea5ab5c37f2fa12032e114967fc442
Shafiquebaloch <muhammadraheem4739733@gmail.com>
2024-12-09 22:48:50 +0500

    commits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,3 @@
-// import Header from "./Component/Header";
-// import Hero from "./Component/hero";
-// import Browse from "./Component/Browse";
-// import OurProduct from "./Component/products";
-// import Inspiration from "./Component/inspiration";
-// import Furniture from "./Component/furniture"
-
-
-// export default function Home() {
-//   return (
-//     <div>
-//       {/* <Header/> */}
-//       <Hero/>
-//       <Browse/>
-//       <OurProduct/>
-//       <Inspiration/>
-//       <Furniture/>
-//       {/* <Show/> */}
-//       {/* <Footer/> */}
-      
-      
-//     </div>
-//   );
-// }
 import  Link  from 'next/link'
 import Image from 'next/image'
 
